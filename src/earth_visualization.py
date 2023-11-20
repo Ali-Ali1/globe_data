7edf2636cdc2110d317dc4b3c08ae278809da92e
Ali Ali <41684444+Ali-Ali1@users.noreply.github.com>
2023-11-19 22:29:54 -0500

    focus on US

--- src/earth_visualization.py
+++ src/earth_visualization.py
@@ -5,23 +5,46 @@ def load_data(filename):
     """Load data from a CSV file."""
     return pd.read_csv(filename)
 
-def plot_3d_earth(data):
-    """Create a 3D plot of the Earth with data points."""
-    trace = go.Scattergeo(
-        lon = data['longitude'],
-        lat = data['latitude'],
+def create_hover_text(row):
+    """Create a text string for hovering."""
+    return f"<b>{row['name']}:</b><br>Yellow ... testing</br>"
+
+def plot_3d_earth(us_states_data):
+    """Create a map focused on the USA with data points."""
+
+    # Add more details to hover text
+    us_states_data['hover_text'] = us_states_data.apply(create_hover_text, axis=1)
+
+    # US States Trace
+    us_states_trace = go.Scattergeo(
+        lon = us_states_data['longitude'],
+        lat = us_states_data['latitude'],
+        hoverinfo = 'text',
+        text = us_states_data['hover_text'],
         mode = 'markers',
-        marker = dict(size = data['magnitude']*10)
+        marker = dict(size = 1, color = 'black'),
+        name = 'US States'
     )
 
     layout = go.Layout(
-        title = '3D Earth Visualization',
-        geo = dict(showland = True, projection = dict(type = 'orthographic'))
+        title = 'Map Focused on the USA',
+        geo = dict(
+            scope = 'usa',
+            showland = True,
+            landcolor = 'green',
+            showocean = True,
+            oceancolor = 'lightblue',
+            showcountries = True,
+            countrycolor = 'black',
+            showsubunits = True,
+            subunitcolor = 'black'
+        ),
+        showlegend = False
     )
 
-    fig = go.Figure(data = [trace], layout = layout)
+    fig = go.Figure(data = [us_states_trace], layout = layout)
     fig.show()
 
 if __name__ == "__main__":
-    data = load_data('data/data.csv') 
-    plot_3d_earth(data)
+    us_states_data = load_data('data/us_states_data.csv') # CSV with additional data for each state
+    plot_3d_earth(us_states_data)
